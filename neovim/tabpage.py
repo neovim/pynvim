ecb69f4a982510a27eef8e9f5958ca74655ce98b
Thiago de Arruda <tpadilha84@gmail.com>
2014-05-13 19:28:02 -0300

    Add tabpage mixin methods

--- neovim/tabpage.py
+++ neovim/tabpage.py
@@ -1,2 +1,21 @@
+from util import RemoteMap
+
 class Tabpage(object):
-    pass
+    @property
+    def vars(self):
+        if not hasattr(self, '_vars'):
+            self._vars = RemoteMap(lambda k: self.get_var(k),
+                                   lambda k, v: self.set_var(k, v))
+        return self._vars
+
+    @property
+    def number(self):
+        return self._handle
+
+    @property
+    def window(self):
+        return self.get_window()
+
+    @property
+    def valid(self):
+        return self.is_valid()
