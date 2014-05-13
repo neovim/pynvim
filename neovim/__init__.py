2eb4fcb2d75e6f93f33b1ae41098919f6d4ebc92
Thiago de Arruda <tpadilha84@gmail.com>
2014-05-13 19:38:17 -0300

    Add helper function for connecting with Neovim

--- neovim/__init__.py
+++ neovim/__init__.py
@@ -1,4 +1,9 @@
 from client import Client
 from uv_stream import UvStream
 
-__all__ = ['Client', 'UvStream', 'c']
+__all__ = ['connect']
+
+def connect(address, port=None):
+    client = Client(UvStream(address, port))
+    client.discover_api()
+    return client.vim
