55e2cae446674ca5cc6152f36e5b8ab6b4f1df65
Thiago de Arruda <tpadilha84@gmail.com>
2014-05-27 16:36:12 -0300

    Add event broadcast tests

--- test/test_events.py
+++ test/test_events.py
@@ -16,3 +16,18 @@ def test_receiving_events():
     eq(events, [['py!', vim.current.buffer.number]])
 
 
+@with_setup(setup=cleanup)
+def test_broadcast():
+    vim.subscribe('event2')
+    vim.command('call send_event(0, "event1", [1,2,3])')
+    vim.command('call send_event(0, "event2", [4,5,6])')
+    vim.command('call send_event(0, "event2", [7,8,9])')
+    events = vim.expect('event2', 2, lambda e: e[1][0] == 7)
+    eq(events, [['event2', [4, 5, 6]], ['event2', [7, 8, 9]]])
+    vim.unsubscribe('event2')
+    vim.subscribe('event1')
+    vim.command('call send_event(0, "event2", [10,11,12])')
+    vim.command('call send_event(0, "event1", [13,14,15])')
+    eq(vim.next_event(), ['event1', [13, 14, 15]])
+
+
