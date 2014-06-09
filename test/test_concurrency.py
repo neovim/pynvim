5e1699823d5e54e1bddc3f080c421bc4a52d9b76
Thiago de Arruda <tpadilha84@gmail.com>
2014-06-09 11:06:51 -0300

    Implement push_event method to enable event loop integration

--- test/test_concurrency.py
+++ test/test_concurrency.py
@@ -7,13 +7,10 @@ from threading import Thread
 
 @with_setup(setup=cleanup)
 def test_concurrent_calls():
-    vim.setup_done = True
     def produce(i):
         str = 'call send_event(%d, "integer", %d)' % (vim.channel_id, i,)
         sleep(0.05 * random())
         vim.command(str)
-
-    vim.subscribe('integer')
 
     count = 50
     for i in xrange(count):
@@ -25,8 +22,34 @@ def test_concurrent_calls():
     while len(integers) < count:
         integers.append(vim.next_event()[1])
 
-    vim.unsubscribe('integer')
-
     for i in xrange(count):
         ok(i in integers)
 
+
+@with_setup(setup=cleanup)
+def test_custom_events():
+    def produce(i):
+        if i % 2 == 0:
+            str = 'call send_event(%d, "nvim-event", 0)' % (vim.channel_id)
+            vim.command(str)
+        else:
+            vim.push_event('custom-event', None)
+        sleep(0.05 * random())
+
+    count = 50
+    for i in xrange(count):
+        t = Thread(target=produce, args=(i,))
+        t.daemon = True
+        t.start()
+
+    nvim_events = []
+    custom_events = []
+    while len(nvim_events) < 25 or len(custom_events) < 25 :
+        event = vim.next_event()
+        if event[0] == 'nvim-event':
+            nvim_events.append(event)
+        elif event[0] == 'custom-event':
+            custom_events.append(event)
+
+    eq(len(nvim_events), 25)
+    eq(len(custom_events), 25)
