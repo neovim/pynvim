c09c4df822a724d06a654bcdd3c4875a718ce80a
Thiago de Arruda <tpadilha84@gmail.com>
2014-07-17 12:08:33 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='neovim',
-      version='0.0.6',
+      version='0.0.7',
       description='Python client to neovim',
       url='http://github.com/neovim/python-client',
-      download_url='https://github.com/neovim/python-client/archive/0.0.6.tar.gz',
+      download_url='https://github.com/neovim/python-client/archive/0.0.7.tar.gz',
       author='Thiago de Arruda',
       author_email='tpadilha84@gmail.com',
       license='MIT',
