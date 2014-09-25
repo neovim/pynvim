523e7a8b4d5347c4f683cc37e05325115b5eba87
Thiago de Arruda <tpadilha84@gmail.com>
2014-09-25 17:01:16 -0300

    Move script_host.py/plugin_host.py to 'plugins' subpackage

---
+++ neovim/plugins/__init__.py
@@ -0,0 +1,5 @@
+from .plugin_host import PluginHost
+from .script_host import ScriptHost
+
+
+__all__ = ['PluginHost', 'ScriptHost']
--- neovim/plugins/script_host.py
+++ neovim/plugins/script_host.py
@@ -1,23 +1,25 @@
-from imp import new_module, find_module, load_module
-import sys, logging, os.path
-from traceback import format_exc
+import imp
+import logging
+import sys
 
 logger = logging.getLogger(__name__)
 debug, warn = (logger.debug, logger.warn,)
 
+
 class ScriptHost(object):
     """
     Plugin that provides the 'python' feature, emulating an environment for
-    python code similar to the one provided by vim-python bindings.
+    python code similar to the one provided by python-vim bindings.
     """
-    def __init__(self, vim):
+    def __init__(self, nvim):
         self.provides = ['python']
-        self.vim = vim
+        self.nvim = nvim
         # context where all code will run
-        self.module = new_module('__main__')
-        vim.script_context = self.module
+        self.module = imp.new_module('__main__')
+        nvim.script_context = self.module
         # it seems some plugins assume 'sys' is already imported, so do it now
         exec('import sys', self.module.__dict__)
+        sys.modules['vim'] = nvim
 
     def python_execute(self, script):
         exec(script, self.module.__dict__)
@@ -28,15 +30,15 @@ class ScriptHost(object):
             exec(script, self.module.__dict__)
 
     def python_do_range(self, start, stop, code):
-        vim = self.vim
+        nvim = self.nvim
         start -= 1
         stop -= 1
         fname = '_vim_pydo'
 
         # Python3 code (exec) must be a string, mixing bytes with
         # function_def would use bytes.__repr__ instead
-        if sys.version_info[0] > 2 and isinstance(code, bytes):
-            code = code.decode(vim.get_option('encoding').decode('ascii'))
+        if isinstance and isinstance(code, bytes):
+            code = code.decode(nvim.options['encoding'].decode('ascii'))
         # define the function
         function_def = 'def %s(line, linenr):\n %s' % (fname, code,)
         exec(function_def, self.module.__dict__)
@@ -49,17 +51,16 @@ class ScriptHost(object):
             # which can be done very quickly in a single API call.
             sstart = start
             sstop = min(start + 5000, stop)
-            lines = vim.current.buffer.get_slice(sstart, sstop, True, True)
+            lines = nvim.current.buffer.get_slice(sstart, sstop, True, True)
             for i, line in enumerate(lines):
                 linenr = i + sstart + 1
                 result = str(function(line, linenr))
                 if result:
                     lines[i] = result
             start = sstop + 1
-            vim.current.buffer.set_slice(sstart, sstop, True, True, lines)
+            nvim.current.buffer.set_slice(sstart, sstop, True, True, lines)
         # delete the function
         del self.module.__dict__[fname]
 
     def python_eval(self, expr):
         return eval(expr, self.module.__dict__)
-
