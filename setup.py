3cbf84f156791abb3c4932b0dc56f9bab4a7d91d
Thiago de Arruda <tpadilha84@gmail.com>
2014-10-01 05:51:55 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -16,10 +16,10 @@ if not platform.python_implementation() == 'PyPy':
     install_requires.append('greenlet')
 
 setup(name='neovim',
-      version='0.0.20',
+      version='0.0.21',
       description='Python client to neovim',
       url='http://github.com/neovim/python-client',
-      download_url='https://github.com/neovim/python-client/archive/0.0.20.tar.gz',
+      download_url='https://github.com/neovim/python-client/archive/0.0.21.tar.gz',
       author='Thiago de Arruda',
       author_email='tpadilha84@gmail.com',
       license='MIT',
