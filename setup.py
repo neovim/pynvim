4845afc0430176e6c9e2fac65fc72d81cb05c120
Thiago de Arruda <tpadilha84@gmail.com>
2015-02-18 16:15:41 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -26,10 +26,10 @@ if not platform.python_implementation() == 'PyPy':
         pass
 
 setup(name='neovim',
-      version='0.0.27',
+      version='0.0.28',
       description='Python client to neovim',
       url='http://github.com/neovim/python-client',
-      download_url='https://github.com/neovim/python-client/archive/0.0.27.tar.gz',
+      download_url='https://github.com/neovim/python-client/archive/0.0.28.tar.gz',
       author='Thiago de Arruda',
       author_email='tpadilha84@gmail.com',
       license='MIT',
