57747f6a5f7ac6b6beb1723ce85438f518b388d1
Björn Linse <bjorn.linse@gmail.com>
2016-02-28 13:48:19 +0100

    add test for threadsafe_call exception and allow logging in tests

--- test/test_concurrency.py
+++ test/test_concurrency.py
@@ -9,3 +9,11 @@ def test_interrupt_from_another_thread():
     timer = Timer(0.5, lambda: session.threadsafe_call(lambda: session.stop()))
     timer.start()
     eq(vim.session.next_message(), None)
+
+
+@with_setup(setup=cleanup)
+def test_exception_in_threadsafe_call():
+    # an exception in a threadsafe_call shouldn't crash the entire host
+    vim.session.threadsafe_call(lambda: [vim.eval("3"), undefined_variable])
+    vim.session.threadsafe_call(lambda: vim.session.stop())
+    vim.session.run(None, None)
