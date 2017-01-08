549f7214e54376dd37a27bbe78c4b5eb2e892508
Hélio Meira Lins <helio@meiralins.org>
2017-01-08 17:16:29 -0300

    event_loop/asyncio.py: Attach loop to child watcher (#241)
    Closes #238

--- neovim/msgpack_rpc/event_loop/asyncio.py
+++ neovim/msgpack_rpc/event_loop/asyncio.py
@@ -93,6 +93,8 @@ class AsyncioEventLoop(BaseEventLoop, asyncio.Protocol,
         self._loop.run_until_complete(coroutine)
 
     def _connect_child(self, argv):
+        self._child_watcher = asyncio.get_child_watcher()
+        self._child_watcher.attach_loop(self._loop)
         coroutine = self._loop.subprocess_exec(self._fact, *argv)
         self._loop.run_until_complete(coroutine)
 
