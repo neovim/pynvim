af652df8c74bc6ac51058e78639ade811ad7be8c
Björn Linse <bjorn.linse@gmail.com>
2018-03-06 17:22:22 +0100

    pynvim 0.2.4

--- setup.py
+++ setup.py
@@ -19,10 +19,10 @@ if platform.python_implementation() != 'PyPy':
     install_requires.append('greenlet')
 
 setup(name='neovim',
-      version='0.2.4dev',
+      version='0.2.4',
       description='Python client to neovim',
       url='http://github.com/neovim/python-client',
-      download_url='https://github.com/neovim/python-client/archive/0.2.3.tar.gz',
+      download_url='https://github.com/neovim/python-client/archive/0.2.4.tar.gz',
       author='Thiago de Arruda',
       author_email='tpadilha84@gmail.com',
       license='Apache',
