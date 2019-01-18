9a613c2993bcf7cc527391cfe3941d55a7988d08
Björn Linse <bjorn.linse@gmail.com>
2019-01-18 10:51:46 +0100

    util: don't add extra newlines in format_exc_skip
    Each item will already end in an newline. This will make a difference when
    we start to use multiline exceptions in Nvim core.

--- pynvim/util.py
+++ pynvim/util.py
@@ -6,10 +6,10 @@ from traceback import format_exception
 
 def format_exc_skip(skip, limit=None):
     """Like traceback.format_exc but allow skipping the first frames."""
-    type, val, tb = sys.exc_info()
+    etype, val, tb = sys.exc_info()
     for i in range(skip):
         tb = tb.tb_next
-    return ('\n'.join(format_exception(type, val, tb, limit))).rstrip()
+    return (''.join(format_exception(etype, val, tb, limit))).rstrip()
 
 
 # Taken from SimpleNamespace in python 3
