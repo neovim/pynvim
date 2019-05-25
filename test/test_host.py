9a4023772388d82d26fb4b7685c8428f1ab9892c
Justin M. Keyes <justinkz@gmail.com>
2019-05-25 14:03:38 +0200

    fixup! session: set client info

--- test/test_host.py
+++ test/test_host.py
@@ -5,7 +5,6 @@ from pynvim.plugin.host import Host, host_method_spec
 def test_host_clientinfo(vim):
     h = Host(vim)
     assert h._request_handlers.keys() == host_method_spec.keys()
-    # TODO: this hangs, why?
-    # assert 'remote' == vim.api.get_chan_info(vim.channel_id)['client']['type']
-    # h.start([])
-    # assert 'host' == vim.api.get_chan_info(vim.channel_id)['client']['type']
+    assert 'remote' == vim.api.get_chan_info(vim.channel_id)['client']['type']
+    h._load([])
+    assert 'host' == vim.api.get_chan_info(vim.channel_id)['client']['type']
