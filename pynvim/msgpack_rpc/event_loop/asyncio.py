4949d43cd45da16c184a15b3f14ebb1f097e8e94
Daniel Hahler <git@thequod.de>
2020-01-14 10:53:30 +0100

    asyncio: pipe_data_received: call _on_error
    `_on_stderr` does not exist.

--- pynvim/msgpack_rpc/event_loop/asyncio.py
+++ pynvim/msgpack_rpc/event_loop/asyncio.py
@@ -68,7 +68,7 @@ class AsyncioEventLoop(BaseEventLoop, asyncio.Protocol,
     def pipe_data_received(self, fd, data):
         """Used to signal `asyncio.SubprocessProtocol` of incoming data."""
         if fd == 2:  # stderr fd number
-            self._on_stderr(data)
+            self._on_error(data)
         elif self._on_data:
             self._on_data(data)
         else:
