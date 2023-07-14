neovim/pynvim

commit eaa862dec340cad523a691d6441e890b800f8b65

fix: imp module is deprecated #527
Co-authored-by: Shougo Matsushita <Shougo.Matsu@gmail.com>

pynvim/compat.py
@@ -1,6 +1,5 @@
 """Code for compatibility across Python versions."""
 import warnings
-from imp import find_module as original_find_module
 from typing import Any, Dict, Optional
 
 

pynvim/plugin/script_host.py
@@ -1,6 +1,5 @@
 # type: ignore
 """Legacy python/python3-vim emulation."""
-import imp
 import io
 import logging
 import os
@@ -202,6 +201,7 @@ def path_hook(nvim):
         return discover_runtime_directories(nvim)
 
     def _find_module(fullname, oldtail, path):
+        import imp
         idx = oldtail.find('.')
         if idx > 0:
             name = oldtail[:idx]
@@ -222,6 +222,7 @@ def path_hook(nvim):
                 return sys.modules[fullname]
             except KeyError:
                 pass
+            import imp
             return imp.load_module(fullname, *self.module)
 
     class VimPathFinder(object):

commit 919217d9211adabd7fd57085657096a5060aece4

fix: undefined name 'original_find_module'
pynvim/compat.py:24:12: F821 undefined name 'original_find_module'

regression from eaa862dec340

pynvim/compat.py
@@ -21,6 +21,7 @@ def find_module(fullname, path):  # type: ignore
             else:
                 newpath.append(element)
         path = newpath
+    from imp import find_module as original_find_module
     return original_find_module(fullname, path)
 
 

pynvim/plugin/host.py
@@ -10,7 +10,6 @@ from traceback import format_exc
 from typing import Any, Sequence
 
 from pynvim.api import Nvim, decode_if_bytes, walk
-from pynvim.compat import find_module
 from pynvim.msgpack_rpc import ErrorResponse
 from pynvim.plugin import script_host
 from pynvim.util import format_exc_skip, get_client_info