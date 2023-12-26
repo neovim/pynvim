950f441b742873fa6202bad94bbeb014b6914335
Jongwook Choi <wookayin@gmail.com>
2023-12-26 12:53:10 -0500

    refactor: add typing for Handler, revise typings in decorators
    - `Handler` is a structured subtype of `Callable` decorated by pynvim
      decorators for RPC handlers; this type provides static typing for the
      private fields internally set by pynvim decorators. This way, we have
      a better static typing and thus no need to rely on `hasattr` checks.
    
    - Add minimal test cases for other decorator types.

--- pynvim/plugin/decorators.py
+++ pynvim/plugin/decorators.py
@@ -1,27 +1,40 @@
 """Decorators used by python host plugin system."""
 
+from __future__ import annotations
+
 import inspect
 import logging
 import sys
-from typing import TYPE_CHECKING, Any, Callable, Dict, Optional, TypeVar, Union
+from functools import partial
+from typing import (TYPE_CHECKING, Any, Callable, Dict, Optional, Type,
+                    TypeVar, Union, cast, overload)
 
 if sys.version_info < (3, 8):
-    from typing_extensions import Literal, TypedDict
+    from typing_extensions import Literal, Protocol, TypedDict
 else:
-    from typing import Literal, TypedDict
+    from typing import Literal, Protocol, TypedDict
 
+if sys.version_info < (3, 10):
+    from typing_extensions import ParamSpec
+else:
+    from typing import ParamSpec
+
+from pynvim.api.common import TDecodeMode
 from pynvim.compat import unicode_errors_default
 
 logger = logging.getLogger(__name__)
-debug, info, warn = (logger.debug, logger.info, logger.warning,)
+debug, info, warn = (
+    logger.debug,
+    logger.info,
+    logger.warning,
+)
 __all__ = ('plugin', 'rpc_export', 'command', 'autocmd', 'function',
            'encoding', 'decode', 'shutdown_hook')
 
 T = TypeVar('T')
-F = TypeVar('F', bound=Callable[..., Any])
-
 
 if TYPE_CHECKING:
+
     class RpcSpec(TypedDict):
         type: Literal['command', 'autocmd', 'function']
         name: str
@@ -29,6 +42,48 @@ if TYPE_CHECKING:
         opts: Any
 else:
     RpcSpec = dict
+
+# type variables for Handler, to represent Callable: P -> R
+P = ParamSpec('P')
+R = TypeVar('R')
+
+
+class Handler(Protocol[P, R]):
+    """An interface to pynvim-decorated RPC handler.
+
+    Handler is basically a callable (method) that is decorated by pynvim.
+    It will have some private fields (prefixed with `_nvim_`), set by
+    decorators that follow below. This generic type allows stronger, static
+    typing for all the private attributes (see `host.Host` for the usage).
+
+    Note: Any valid Handler that is created by pynvim's decorator is guaranteed
+    to have *all* of the following `_nvim_*` attributes defined as per the
+    "Protocol", so there is NO need to check `hasattr(handler, "_nvim_...")`.
+    Exception is _nvim_decode; this is an optional attribute orthgonally set by
+    the decorator `@decode()`.
+    """
+    __call__: Callable[P, R]
+
+    _nvim_rpc_method_name: str
+    _nvim_rpc_sync: bool
+    _nvim_bind: bool
+    _nvim_prefix_plugin_path: bool
+    _nvim_rpc_spec: Optional[RpcSpec]
+    _nvim_shutdown_hook: bool
+
+    _nvim_registered_name: Optional[str]  # set later by host when discovered
+
+    @classmethod
+    def wrap(cls, fn: Callable[P, R]) -> Handler[P, R]:
+        fn = cast(Handler[P, R], partial(fn))
+        fn._nvim_bind = False
+        fn._nvim_rpc_method_name = None  # type: ignore
+        fn._nvim_rpc_sync = None  # type: ignore
+        fn._nvim_prefix_plugin_path = False
+        fn._nvim_rpc_spec = None
+        fn._nvim_shutdown_hook = False
+        fn._nvim_registered_name = None
+        return fn
 
 
 def plugin(cls: T) -> T:
@@ -38,24 +93,34 @@ def plugin(cls: T) -> T:
     plugin_load method of the host.
     """
     cls._nvim_plugin = True  # type: ignore[attr-defined]
+
     # the _nvim_bind attribute is set to True by default, meaning that
     # decorated functions have a bound Nvim instance as first argument.
     # For methods in a plugin-decorated class this is not required, because
     # the class initializer will already receive the nvim object.
-    predicate = lambda fn: hasattr(fn, '_nvim_bind')
+    predicate = lambda fn: getattr(fn, '_nvim_bind', False)
     for _, fn in inspect.getmembers(cls, predicate):
         fn._nvim_bind = False
     return cls
 
 
-def rpc_export(rpc_method_name: str, sync: bool = False) -> Callable[[F], F]:
+def rpc_export(
+    rpc_method_name: str,
+    sync: bool = False,
+) -> Callable[[Callable[P, R]], Handler[P, R]]:
     """Export a function or plugin method as a msgpack-rpc request handler."""
-    def dec(f: F) -> F:
-        f._nvim_rpc_method_name = rpc_method_name  # type: ignore[attr-defined]
-        f._nvim_rpc_sync = sync  # type: ignore[attr-defined]
-        f._nvim_bind = True  # type: ignore[attr-defined]
-        f._nvim_prefix_plugin_path = False  # type: ignore[attr-defined]
+
+    def dec(f: Callable[P, R]) -> Handler[P, R]:
+        f = cast(Handler[P, R], f)
+        f._nvim_rpc_method_name = rpc_method_name
+        f._nvim_rpc_sync = sync
+        f._nvim_bind = True
+        f._nvim_prefix_plugin_path = False
+        f._nvim_rpc_spec = None  # not used
+        f._nvim_shutdown_hook = False  # not used
+        f._nvim_registered_name = None  # TBD
         return f
+
     return dec
 
 
@@ -70,15 +135,15 @@ def command(
     sync: bool = False,
     allow_nested: bool = False,
     eval: Optional[str] = None
-) -> Callable[[F], F]:
+) -> Callable[[Callable[P, R]], Handler[P, R]]:
     """Tag a function or plugin method as a Nvim command handler."""
-    def dec(f: F) -> F:
-        f._nvim_rpc_method_name = (  # type: ignore[attr-defined]
-            'command:{}'.format(name)
-        )
-        f._nvim_rpc_sync = sync  # type: ignore[attr-defined]
-        f._nvim_bind = True  # type: ignore[attr-defined]
-        f._nvim_prefix_plugin_path = True  # type: ignore[attr-defined]
+
+    def dec(f: Callable[P, R]) -> Handler[P, R]:
+        f = cast(Handler[P, R], f)
+        f._nvim_rpc_method_name = ('command:{}'.format(name))
+        f._nvim_rpc_sync = sync
+        f._nvim_bind = True
+        f._nvim_prefix_plugin_path = True
 
         opts: Dict[str, Any] = {}
 
@@ -107,13 +172,16 @@ def command(
         else:
             rpc_sync = sync
 
-        f._nvim_rpc_spec = {  # type: ignore[attr-defined]
+        f._nvim_rpc_spec = {
             'type': 'command',
             'name': name,
             'sync': rpc_sync,
             'opts': opts
         }
+        f._nvim_shutdown_hook = False
+        f._nvim_registered_name = None  # TBD
         return f
+
     return dec
 
 
@@ -123,19 +191,17 @@ def autocmd(
     sync: bool = False,
     allow_nested: bool = False,
     eval: Optional[str] = None
-) -> Callable[[F], F]:
+) -> Callable[[Callable[P, R]], Handler[P, R]]:
     """Tag a function or plugin method as a Nvim autocommand handler."""
-    def dec(f: F) -> F:
-        f._nvim_rpc_method_name = (  # type: ignore[attr-defined]
-            'autocmd:{}:{}'.format(name, pattern)
-        )
-        f._nvim_rpc_sync = sync  # type: ignore[attr-defined]
-        f._nvim_bind = True  # type: ignore[attr-defined]
-        f._nvim_prefix_plugin_path = True  # type: ignore[attr-defined]
 
-        opts = {
-            'pattern': pattern
-        }
+    def dec(f: Callable[P, R]) -> Handler[P, R]:
+        f = cast(Handler[P, R], f)
+        f._nvim_rpc_method_name = ('autocmd:{}:{}'.format(name, pattern))
+        f._nvim_rpc_sync = sync
+        f._nvim_bind = True
+        f._nvim_prefix_plugin_path = True
+
+        opts = {'pattern': pattern}
 
         if eval:
             opts['eval'] = eval
@@ -145,13 +211,16 @@ def autocmd(
         else:
             rpc_sync = sync
 
-        f._nvim_rpc_spec = {  # type: ignore[attr-defined]
+        f._nvim_rpc_spec = {
             'type': 'autocmd',
             'name': name,
             'sync': rpc_sync,
             'opts': opts
         }
+        f._nvim_shutdown_hook = False
+        f._nvim_registered_name = None  # TBD
         return f
+
     return dec
 
 
@@ -161,15 +230,15 @@ def function(
     sync: bool = False,
     allow_nested: bool = False,
     eval: Optional[str] = None
-) -> Callable[[F], F]:
+) -> Callable[[Callable[P, R]], Handler[P, R]]:
     """Tag a function or plugin method as a Nvim function handler."""
-    def dec(f: F) -> F:
-        f._nvim_rpc_method_name = (  # type: ignore[attr-defined]
-            'function:{}'.format(name)
-        )
-        f._nvim_rpc_sync = sync  # type: ignore[attr-defined]
-        f._nvim_bind = True  # type: ignore[attr-defined]
-        f._nvim_prefix_plugin_path = True  # type: ignore[attr-defined]
+
+    def dec(f: Callable[P, R]) -> Handler[P, R]:
+        f = cast(Handler[P, R], f)
+        f._nvim_rpc_method_name = ('function:{}'.format(name))
+        f._nvim_rpc_sync = sync
+        f._nvim_bind = True
+        f._nvim_prefix_plugin_path = True
 
         opts = {}
 
@@ -184,37 +253,79 @@ def function(
         else:
             rpc_sync = sync
 
-        f._nvim_rpc_spec = {  # type: ignore[attr-defined]
+        f._nvim_rpc_spec = {
             'type': 'function',
             'name': name,
             'sync': rpc_sync,
             'opts': opts
         }
+        f._nvim_shutdown_hook = False  # not used
+        f._nvim_registered_name = None  # TBD
         return f
+
     return dec
 
 
-def shutdown_hook(f: F) -> F:
+def shutdown_hook(f: Callable[P, R]) -> Handler[P, R]:
     """Tag a function or method as a shutdown hook."""
-    f._nvim_shutdown_hook = True  # type: ignore[attr-defined]
-    f._nvim_bind = True  # type: ignore[attr-defined]
+    f = cast(Handler[P, R], f)
+    f._nvim_rpc_method_name = ''  # Falsy value, not used
+    f._nvim_rpc_sync = True  # not used
+    f._nvim_prefix_plugin_path = False  # not used
+    f._nvim_rpc_spec = None  # not used
+
+    f._nvim_shutdown_hook = True
+    f._nvim_bind = True
+    f._nvim_registered_name = None  # TBD
     return f
 
 
-def decode(mode: str = unicode_errors_default) -> Callable[[F], F]:
-    """Configure automatic encoding/decoding of strings."""
-    def dec(f: F) -> F:
-        f._nvim_decode = mode  # type: ignore[attr-defined]
+T_Decode = Union[Type, Handler[P, R]]
+
+
+def decode(
+    mode: TDecodeMode = unicode_errors_default,
+) -> Callable[[T_Decode], T_Decode]:
+    """Configure automatic encoding/decoding of strings.
+
+    This decorator can be put around an individual Handler (@rpc_export,
+    @autocmd, @function, @command, or @shutdown_hook), or around a class
+    (@plugin, has an effect on all the methods unless overridden).
+
+    The argument `mode` will be passed as an argument to:
+        bytes.decode("utf-8", errors=mode)
+    when decoding bytestream Nvim RPC responses.
+
+    See https://docs.python.org/3/library/codecs.html#error-handlers for
+    the list of valid modes (error handler values).
+
+    See also:
+        pynvim.api.Nvim.with_decode(mode)
+        pynvim.api.common.decode_if_bytes(..., mode)
+    """
+
+    @overload
+    def dec(f: Handler[P, R]) -> Handler[P, R]:
+        ...  # decorator on method
+
+    @overload
+    def dec(f: Type[T]) -> Type[T]:
+        ...  # decorator on class
+
+    def dec(f):  # type: ignore
+        f._nvim_decode = mode
         return f
-    return dec
+
+    return dec  # type: ignore
 
 
-def encoding(encoding: Union[bool, str] = True) -> Callable[[F], F]:
+def encoding(encoding: Union[bool, str] = True):  # type: ignore
     """DEPRECATED: use pynvim.decode()."""
     if isinstance(encoding, str):
         encoding = True
 
-    def dec(f: F) -> F:
-        f._nvim_decode = encoding  # type: ignore[attr-defined]
+    def dec(f):  # type: ignore
+        f._nvim_decode = encoding if encoding else None
         return f
+
     return dec
--- pynvim/plugin/host.py
+++ pynvim/plugin/host.py
@@ -13,7 +13,7 @@ from functools import partial
 from traceback import format_exc
 from types import ModuleType
 from typing import (Any, Callable, Dict, List, Optional, Sequence, Type,
-                    TypeVar, Union)
+                    TypeVar, Union, cast)
 
 from pynvim.api import Nvim, decode_if_bytes, walk
 from pynvim.api.common import TDecodeMode
@@ -31,6 +31,9 @@ host_method_spec = {"poll": {}, "specs": {"nargs": 1}, "shutdown": {}}
 
 
 T = TypeVar('T')
+
+RpcSpec = decorators.RpcSpec
+Handler = decorators.Handler
 
 
 def _handle_import(path: str, name: str) -> ModuleType:
@@ -53,7 +56,7 @@ class Host:
     Takes care of loading/unloading plugins and routing msgpack-rpc
     requests/notifications to the appropriate handlers.
     """
-    _specs: Dict[str, list[decorators.RpcSpec]]  # path -> rpc spec
+    _specs: Dict[str, list[RpcSpec]]  # path -> list[ rpc handler spec ]
     _loaded: Dict[str, dict]  # path -> {handlers: ..., modules: ...}
     _load_errors: Dict[str, str]  # path -> error message
 
@@ -63,13 +66,13 @@ class Host:
         self._specs = {}
         self._loaded = {}
         self._load_errors = {}
-        self._notification_handlers: Dict[str, Callable] = {
-            'nvim_error_event': self._on_error_event
+        self._notification_handlers: Dict[str, Handler] = {
+            'nvim_error_event': Handler.wrap(self._on_error_event),
         }
-        self._request_handlers: Dict[str, Callable] = {
-            'poll': lambda: 'ok',
-            'specs': self._on_specs_request,
-            'shutdown': self.shutdown
+        self._request_handlers: Dict[str, Handler] = {
+            'poll': Handler.wrap(lambda: 'ok'),
+            'specs': Handler.wrap(self._on_specs_request),
+            'shutdown': Handler.wrap(self.shutdown),
         }
 
         self._decode_default = True
@@ -100,17 +103,18 @@ class Host:
     def _wrap_delayed_function(
         self,
         cls: Type[T],  # a class type
-        delayed_handlers: List[Callable],
+        delayed_handlers: List[Handler],
         name: str,
         sync: bool,
-        module_handlers: List[Callable],
+        module_handlers: List[Handler],
         path: str,
         *args: Any,
     ) -> Any:
         # delete the delayed handlers to be sure
         for handler in delayed_handlers:
-            method_name = handler._nvim_registered_name  # type: ignore[attr-defined]
-            if handler._nvim_rpc_sync:  # type: ignore[attr-defined]
+            method_name = handler._nvim_registered_name
+            assert method_name is not None
+            if handler._nvim_rpc_sync:
                 del self._request_handlers[method_name]
             else:
                 del self._notification_handlers[method_name]
@@ -210,7 +214,7 @@ class Host:
                 else:
                     directory, name = os.path.split(os.path.splitext(path)[0])
                     module = _handle_import(directory, name)
-                handlers: List[Callable] = []
+                handlers: List[Handler] = []
                 self._discover_classes(module, handlers, path)
                 self._discover_functions(module, handlers, path, delay=False)
                 if not handlers:
@@ -234,7 +238,7 @@ class Host:
             handlers = plugin['handlers']
             for handler in handlers:
                 method_name = handler._nvim_registered_name
-                if hasattr(handler, '_nvim_shutdown_hook'):
+                if handler._nvim_shutdown_hook:
                     handler()
                 elif handler._nvim_rpc_sync:
                     del self._request_handlers[method_name]
@@ -246,7 +250,7 @@ class Host:
     def _discover_classes(
         self,
         module: ModuleType,
-        handlers: List[Callable],
+        handlers: List[Handler],
         plugin_path: str,
     ) -> None:
         for _, cls in inspect.getmembers(module, inspect.isclass):
@@ -257,18 +261,19 @@ class Host:
     def _discover_functions(
         self,
         obj: Union[Type, ModuleType, Any],  # class, module, or plugin instance
-        handlers: List[Callable],
+        handlers: List[Handler],
         plugin_path: str,
         delay: bool,
     ) -> None:
         def predicate(o: Any) -> bool:
-            return hasattr(o, '_nvim_rpc_method_name')
+            return bool(getattr(o, '_nvim_rpc_method_name', False))
 
-        cls_handlers: List[Callable] = []
+        cls_handlers: List[Handler] = []
         specs: List[decorators.RpcSpec] = []
-        obj_decode: TDecodeMode = getattr(obj, '_nvim_decode',
-                                          self._decode_default)  # type: ignore
+        obj_decode: TDecodeMode = cast(
+            TDecodeMode, getattr(obj, '_nvim_decode', self._decode_default))
         for _, fn in inspect.getmembers(obj, predicate):
+            fn = cast(Handler, fn)  # because hasattr(_nvim_rpc_method_name)
             method: str = fn._nvim_rpc_method_name
             if fn._nvim_prefix_plugin_path:
                 method = '{}:{}'.format(plugin_path, method)
@@ -276,34 +281,33 @@ class Host:
             if delay:
                 # TODO: Fix typing on obj. delay=True assumes obj is a class!
                 assert isinstance(obj, type), "obj must be a class type"
-                fn_wrapped = partial(self._wrap_delayed_function, obj,
-                                     cls_handlers, method, sync,
-                                     handlers, plugin_path)
+                _fn_wrapped = partial(self._wrap_delayed_function, obj,
+                                      cls_handlers, method, sync,
+                                      handlers, plugin_path)
             else:
                 decode: TDecodeMode = getattr(fn, '_nvim_decode', obj_decode)
                 nvim_bind: Optional[Nvim] = None
                 if fn._nvim_bind:
                     nvim_bind = self._configure_nvim_for(fn)
 
-                fn_wrapped = partial(self._wrap_function, fn,
-                                     sync, decode, nvim_bind, method)
-            self._copy_attributes(fn, fn_wrapped)
-            fn_wrapped._nvim_registered_name = method  # type: ignore[attr-defined]
+                _fn_wrapped = partial(self._wrap_function, fn,
+                                      sync, decode, nvim_bind, method)
+            self._copy_attributes(fn, _fn_wrapped)
+            fn_wrapped: Handler = cast(Handler, _fn_wrapped)
+            fn_wrapped._nvim_registered_name = method
 
             # register in the rpc handler dict
             if sync:
                 if method in self._request_handlers:
-                    raise Exception(
-                        f'Request handler for "{method}" is already registered'
-                    )
+                    raise Exception(f'Request handler for "{method}" '
+                                    'is already registered')
                 self._request_handlers[method] = fn_wrapped
             else:
                 if method in self._notification_handlers:
-                    raise Exception(
-                        f'Notification handler for "{method}" is already registered'
-                    )
+                    raise Exception(f'Notification handler for "{method}" '
+                                    'is already registered')
                 self._notification_handlers[method] = fn_wrapped
-            if hasattr(fn, '_nvim_rpc_spec'):
+            if fn._nvim_rpc_spec:
                 specs.append(fn._nvim_rpc_spec)
             handlers.append(fn_wrapped)
             cls_handlers.append(fn_wrapped)
@@ -317,7 +321,7 @@ class Host:
                 setattr(dst, attr, getattr(src, attr))
 
     def _on_specs_request(self, path: Union[str, bytes]
-                          ) -> List[decorators.RpcSpec]:
+                          ) -> List[RpcSpec]:
         path = decode_if_bytes(path)
         assert isinstance(path, str)
         if path in self._load_errors:
@@ -327,7 +331,8 @@ class Host:
     def _configure_nvim_for(self, obj: Any) -> Nvim:
         # Configure a nvim instance for obj (checks encoding configuration)
         nvim = self.nvim
-        decode = getattr(obj, '_nvim_decode', self._decode_default)
+        decode: TDecodeMode = cast(
+            TDecodeMode, getattr(obj, '_nvim_decode', self._decode_default))
         if decode:
             nvim = nvim.with_decode(decode)
         return nvim
