68250cb55e749151541e60a92757d53ece5d720f
John te Bokkel <tanj.tanj@gmail.com>
2024-04-13 18:38:45 -0700

    fix: take a hybrid approach to restore tests
    Path.resolve() also returns the absolute path, while os.path.normpath
    did not. We need to both normalize the path and convert it to standard
    posix style to fix the issue.

--- pynvim/plugin/host.py
+++ pynvim/plugin/host.py
@@ -5,6 +5,7 @@ import importlib
 import inspect
 import logging
 import os
+import os.path
 import pathlib
 import re
 import sys
@@ -173,7 +174,7 @@ class Host(object):
         # self.nvim.err_write("host init _load\n", async_=True)
         has_script = False
         for path in plugins:
-            path = pathlib.Path(path).resolve().as_posix()  # normalize path
+            path = pathlib.Path(os.path.normpath(path)).as_posix()  # normalize path
             err = None
             if path in self._loaded:
                 warn('{} is already loaded'.format(path))
@@ -276,7 +277,7 @@ class Host(object):
 
     def _on_specs_request(self, path):
         path = decode_if_bytes(path)
-        path = pathlib.Path(path).resolve().as_posix()  # normalize path
+        path = pathlib.Path(os.path.normpath(path)).as_posix()  # normalize path
         if path in self._load_errors:
             self.nvim.out_write(self._load_errors[path] + '\n')
         return self._specs.get(path, 0)
